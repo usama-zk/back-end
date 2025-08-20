8552db6122e0131766b58a5e3a5fa9b4d86f0f77
Usama <usama.mehmood2002@gmail.com>
2025-08-20 19:09:28 +0500

    connected to db

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// import mongoose from "mongoose";
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Error", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     throw error;
+//   }
+// })();
+require("dotenv").config();
+const connectDB = require("./db/index.js");
+connectDB();
